17fcaeb98655880ca144d2a671ff6899f0a6cce5
Jeroen de Weerdt <jeroen.de.weerdt@xs4all.nl>
2014-02-02 17:51:59 +0100

    Added a few comments

--- UKHASnetIgate.py
+++ UKHASnetIgate.py
@@ -1,18 +1,14 @@
 import urllib
 import urllib2
 import serial
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-
-# API (POST) http://ukhas.net/api/upload
-# origin: [callsign of gateway node]
-# data: [raw data packet]
+ser = serial.Serial('/dev/ttyUSB0', 9600) # Serial connection settings go here
 
 while True:
-  rawpkt = ser.readline().rstrip()
+  rawpkt = ser.readline().rstrip() # incoming data, one line at the time, stripped from all kind of whitespaces
   print ('Incoming raw: ' + rawpkt)
 
   url = 'http://ukhas.net/api/upload'
-  values = {'origin' : 'REVSPACE00', 'data' : rawpkt}
+  values = {'origin' : 'REVSPACE00', 'data' : rawpkt} # origin, who submitted it to the API?
 
   data = urllib.urlencode(values)
   req = urllib2.Request(url, data)
