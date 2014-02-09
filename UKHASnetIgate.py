b35c5586f989d7448265ede97d0cfbb1c64539bb
Jeroen de Weerdt <jeroen.de.weerdt@xs4all.nl>
2014-02-09 20:51:30 +0100

    Incoming messages now include timestamps, we also now indent correcly.

--- UKHASnetIgate.py
+++ UKHASnetIgate.py
@@ -1,14 +1,15 @@
 import urllib
 import urllib2
 import serial
+import time
 ser = serial.Serial('/dev/ttyUSB0', 9600) # Serial connection settings go here
 
 while True:
   rawpkt = ser.readline().rstrip() # incoming data, one line at the time, stripped from all kind of whitespaces
-  print ('Incoming raw: ' + rawpkt)
+  print (time.strftime("%c") + '\n\tIncoming raw: \t' + rawpkt)
 
   strppkt = rawpkt.strip('rx: ') # remove the 'rx: ' at the beginning
-  print ('Sending to API: ' + strppkt)
+  print ('\tSending to API: ' + strppkt)
 
   url = 'http://ukhas.net/api/upload'
   values = {'origin' : 'RS00', 'data' : strppkt} # origin, who submitted it to the API?
@@ -17,4 +18,4 @@ while True:
   req = urllib2.Request(url, data)
   response = urllib2.urlopen(req)
   the_page = response.read()
-  print ('API response: ' + the_page + '\n')
+  print ('\tAPI response: \t' + the_page + '\n')
