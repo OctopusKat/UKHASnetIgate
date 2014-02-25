4c4912160981432b23f18424121b5e6e03b1fd33
maxell <maxell@soms.echtehackers.nl>
2014-02-25 17:14:18 +0100

    wWe now handle erros correctly, also closing the script will now happen without any errors. Please note that the default ttyUSB has changed.

--- UKHASnetIgate.py
+++ UKHASnetIgate.py
@@ -2,35 +2,39 @@ import urllib
 import urllib2
 import serial
 import time
-import atexit
+import sys
 
-serialbuf = serial.Serial('/dev/ttyUSB0', 9600) # Configured for my setup
+serialbuf = serial.Serial('/dev/ttyUSB1', 9600) # Configured for my setup
 igatename = 'RS00' # should be the same as it's RF-id
 
-def exit_handler():
-  print 'Clean exit. Bye bye!'
+try:
+  while True:
+    try:
+      rawpacket = serialbuf.readline().rstrip() # We should look at the incoming data one line at the time
+      print (time.strftime("%c")) # Timestamp in "Sun Feb  9 21:51:48 2014" format
 
-atexit.register(exit_handler)
+      if rawpacket.startswith('rx: '): # We are only intrested in received RF packets
+        print ('\tIncoming raw: \t' + rawpacket)
 
+        strippacket = rawpacket.strip('rx: ') # Remove the prefix
+        print ('\tSending to API: ' + strippacket)
 
-while True:
-  rawpacket = serialbuf.readline().rstrip() # We should look at the incoming data one line at the time
-  print (time.strftime("%c")) # Timestamp in "Sun Feb  9 21:51:48 2014" format
+        url = 'http://ukhas.net/api/upload'
+        values = {'origin' : igatename, 'data' : strippacket} # API requires origin and data
 
-  if rawpacket.startswith('rx: '): # We are only intrested in received RF packets
-    print ('\tIncoming raw: \t' + rawpacket)
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
+        apifeedback = response.read().rstrip() # Once again we clean it up before parsing
+        print ('\tAPI response: \t' + apifeedback + '\n')
 
-    strippacket = rawpacket.strip('rx: ') # Remove the prefix
-    print ('\tSending to API: ' + strippacket)
+      else:
+        print ('\tIncoming raw: \t\033[1m' + rawpacket + '\033[0m\n')
 
-    url = 'http://ukhas.net/api/upload'
-    values = {'origin' : igatename, 'data' : strippacket} # API requires origin and data
+    except Exception, e:
+      continue
 
-    data = urllib.urlencode(values)
-    req = urllib2.Request(url, data)
-    response = urllib2.urlopen(req)
-    apifeedback = response.read().rstrip() # Once again we clean it up before parsing
-    print ('\tAPI response: \t' + apifeedback + '\n') # error checking goes here
-
-  else:
-    print ('\tIncoming raw: \t\033[1m' + rawpacket + '\033[0m\n')
+except KeyboardInterrupt:
+    print '\nClean exit. Bye bye!'
+    serialbuf.close() # Close serial port
+    sys.exit()
